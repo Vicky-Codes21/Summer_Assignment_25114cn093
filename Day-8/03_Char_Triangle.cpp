eb3d8064b2910af16abe9d510ea1b8cd0474ace7
Vicky-Codes21 <vky45615@gmail.com>
2026-06-08 19:59:19 +0530

    Day 8 problems.

---
+++ Day-8/03_Char_Triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Enter number of row : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(char j='A'; j<='A'+i-1; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
