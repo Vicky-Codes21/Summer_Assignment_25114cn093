8b6208fa733a5a6cb8aa545efb477d86f3da1f1f
Vicky-Codes21 <vky45615@gmail.com>
2026-06-23 16:13:55 +0530

    Day 17 problems.

---
+++ Day-17/02_Union_arr.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n1,n2;
+    cout<<"Enter the size of first array : ";
+    cin>>n1;
+    int arr1[n1];
+    cout<<"Enter elements : ";
+    for(int i=0;i<n1;i++){
+        cin>>arr1[i];
+    }   
+    cout<<"Enter the size of second array : ";
+    cin>>n2;
+    int arr2[n2];
+    cout<<"Enter elements : ";
+    for(int i=0;i<n2;i++){
+        cin>>arr2[i];
+    }
+    int merged[n1+n2];
+    int k=0;
+    for(int i=0;i<n1;i++){
+        merged[k]=arr1[i];
+        k++;
+    }
+    for(int i=0;i<n2;i++){
+        merged[k]=arr2[i];
+        k++;
+    }
+    cout<<"Array after taking union : ";
+    for(int i=0;i<(n1+n2);i++){
+        bool duplicate=false;
+        for(int j=0;j<i;j++){
+            if (merged[i]==merged[j]){
+                duplicate=true;
+                break;
+            }
+        }
+        if(!duplicate){
+            cout<<merged[i]<<" ";
+        }
+    }
+    return 0;
+}
